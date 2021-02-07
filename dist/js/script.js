b56124545c2dff2d1bbac7f111a709e289545841
Lukina Olga <Lookinolga@gmail.com>
2021-02-08 01:06:38 +0300

    update5

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,18 +1,61 @@
 'use strict';
 
- window.addEventListener('DOMContentLoaded', () => {
-    const burger = document.querySelector('.header__burger');
-    const link = document.querySelectorAll('.header__link');
+window.addEventListener('DOMContentLoaded', () => {
+    
+    class menuBurger {
+        constructor(selector, menuItemsParent) {
+            this.selector = selector;
+            this.menuItemsParent = menuItemsParent;
+        }
 
-    burger.addEventListener('click', () => {
-        document.body.classList.toggle('menu-active');
-    })
+        init() {
+            this.selector = document.querySelector(this.selector);
+            this.menuItemsParent = document.querySelector(this.menuItemsParent);
 
-    link.forEach(link => {
-        link.addEventListener('click', () => {
-            document.body.classList.toggle('menu-active');
-        })
-    })
+            this.selector.addEventListener('click', () => {
+                // if(document.body.classList.contains('menu-active')) {
+                //     document.body.style.paddingRight = `0px`;
+                // } else {
+                //     document.body.style.paddingRight = `${this.getScrollBarWidth()}px`;
+                // }
+                
+                document.body.classList.toggle('menu-active');
+            })
+
+            this.menuItemsParent.addEventListener('click', (e) => {
+                let target = e.target;
+
+                if (target.tagName === 'A') {
+                    document.body.classList.toggle('menu-active');
+                }
+            })
+        }
+
+        getScrollBarWidth() {
+            const bar = document.createElement('div');
+
+            bar.style.cssText = `
+                position: absolute;
+                top: -10000px;
+                width: 50px;
+                height: 50px;
+                overflow: scroll;
+                visibility: hidden;
+            `;
+
+            document.body.appendChild(bar);
+
+            const scrollBarWidth = bar.offsetWidth - bar.clientWidth;
+     
+            document.body.removeChild(bar);
+
+            return scrollBarWidth;
+            
+        }
+    }
+
+    new menuBurger('.header__burger', '.header__items').init();
+
 
     class Slider {
         constructor(obj) {
@@ -25,30 +68,29 @@
             this.position = 0;
             this.width = null;
         }
-        
+
         init() {
             this.slides = document.querySelectorAll(this.slides);
             this.slider = document.querySelector(this.slider);
             this.wrapper = document.querySelector(this.wrapper);
 
             this.track = document.querySelector(this.track);
-            
-           
+
             this.calc();
 
-            if(this.pagination) {
+            if (this.pagination) {
                 const dots = document.createElement('ul'),
-                controls = [];
+                    controls = [];
                 dots.classList.add('slider-dots');
                 this.slider.append(dots);
 
                 let slidesLength = this.slides.length;
-                for(let i = 0; i < slidesLength; i++) {
+                for (let i = 0; i < slidesLength; i++) {
                     const dot = document.createElement('li');
                     dot.setAttribute('data-slide-to', i);
                     dot.classList.add('dot');
 
-                    if(i === 0) {
+                    if (i === 0) {
                         dot.classList.add('dot-active');
                     }
                     dots.append(dot);
@@ -58,17 +100,17 @@
                 dots.addEventListener('click', (e) => {
                     let target = e.target;
 
-                    if(target.tagName === 'LI') {
+                    if (target.tagName === 'LI') {
                         const slideTo = target.getAttribute('data-slide-to');
-                        
+
                         controls.forEach(dot => dot.classList.remove('dot-active'));
                         target.classList.add('dot-active');
                         this.index = slideTo;
                         this.position = this.deleteNodigits(this.width) * this.index;
                         this.track.style.transition = 'transform 0.5s';
                         this.movePosition();
-    
-                    } 
+
+                    }
                 })
             }
 
@@ -100,14 +142,14 @@
     }
 
 
-const slider = document.querySelectorAll('.slider');
+    const slider = document.querySelectorAll('.slider');
 
-    if(slider.length !== 0 ) 
-    new Slider({
-        slider: '.slider',
-        slides: '.slide',
-        wrapper: '.slider-wrapper' ,
-        track: '.slider-track',
-        pagination: true,
-    }).init();
- })
+    if (slider.length !== 0)
+        new Slider({
+            slider: '.slider',
+            slides: '.slide',
+            wrapper: '.slider-wrapper',
+            track: '.slider-track',
+            pagination: true,
+        }).init();
+})
